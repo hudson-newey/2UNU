ef0e593172c42543c7b4237bff1ab988e82ccdca
hudson-newey <neweyh@qut.edu.au>
2023-06-17 16:42:50 +1000

    added arch

---
+++ arch/arch.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "constants.h"
+
+void readFile(char *fileName) {
+    char c;
+    FILE *file = fopen(fileName, "r");
+
+    while ((c = getc(file)) != EOF) {
+        printf("%c", c);
+    }
+
+    fclose(file);
+}
+
+int main(int argc, char const *argv[]) {
+    if (argc > 1) {
+        if (strcmp(argv[1], "--version") == 0) {
+            printf("%s\n", version);
+            return 0;
+         } else if (strcmp(argv[1], "--help") == 0) {
+            printf("%s\n", helpDocs);
+            return 0;
+        }
+    }
+
+    // TODO: I should probably parse the /proc/cpuinfo file to keep it in spec with GNU
+    readFile("/proc/cpuinfo");
+
+    return 0;
+}
--- constants.h
+++
@@ -1,6 +0,0 @@
-#define false 0
-#define true 1
-
-#define twoUnuVersion "0.0.1"
-
-const char newLineCharacter[8] = "\n";
