ac071b7494bf09f39465a05b2d77c82be5431cb2
hudson-newey <neweyh@qut.edu.au>
2023-08-05 21:10:18 +1000

    Added env command

---
+++ env/env.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "env.h"
+
+int main(int argc, const char *argv[], char *envp[]) {
+    if (argc > 1) {
+        if (strcmp(argv[1], "--version") == 0) {
+            printf("%s\n", version);
+            return 0;
+        } else if (strcmp(argv[1], "--help") == 0) {
+            printf("%s\n", helpDocs);
+            return 0;
+        }
+
+        printf("env: unrecognized option '%s'", argv[1]);
+        return 1;
+    }
+
+    for (int i = 0; envp[i] != NULL; i++) {
+        printf("%s\n", envp[i]);
+    }
+
+    return 0;
+}
