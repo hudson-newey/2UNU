6e614e1a48aece9146cb42503b29273bf44fe6a4
hudson-newey <neweyh@qut.edu.au>
2023-08-08 21:32:57 +1000

    Added base64 command

---
+++ base64/base64.c
@@ -0,0 +1,168 @@
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include "base64.h"
+
+#define BUFFER_SIZE 1024
+#define IO_STREAM_TEMP_FILE "/tmp/temp.txt"
+
+bool fileExists(const char *filePath)
+{
+    struct stat buffer;
+    return (stat(filePath, &buffer) == 0);
+}
+
+void writeToFile(char *filePath, char contents[][BUFFER_SIZE])
+{
+    FILE *filePointer = fopen(filePath, "w");
+    if (filePointer == NULL)
+    {
+        printf("Error: could not open file %s\n", filePath);
+        return;
+    }
+
+    for (unsigned int i = 0; contents[i][0] != '\0'; i++)
+    {
+        fprintf(filePointer, "%s", contents[i]);
+    }
+
+    fclose(filePointer);
+}
+
+void deleteFile(char *filePath)
+{
+    if (fileExists(filePath))
+    {
+        remove(filePath);
+    }
+    else
+    {
+        printf("Error: failed to delete temp file, file %s does not exist\n", filePath);
+    }
+}
+
+char *readFile(const char *filePath)
+{
+    FILE *f = fopen(filePath, "rb");
+
+    // f invalid? fseek() fail?
+    if (f == NULL || fseek(f, 0, SEEK_END))
+    {
+        return NULL;
+    }
+
+    long length = ftell(f);
+    rewind(f);
+    // Did ftell() fail?  Is the length too long?
+    if (length == -1 || (unsigned long)length >= SIZE_MAX)
+    {
+        return NULL;
+    }
+
+    // Convert from long to size_t
+    size_t ulength = (size_t)length;
+    char *buffer = malloc(ulength + 1);
+    // Allocation failed? Read incomplete?
+    if (buffer == NULL || fread(buffer, 1, ulength, f) != ulength)
+    {
+        free(buffer);
+        return NULL;
+    }
+    buffer[ulength] = '\0'; // Now buffer points to a string
+
+    return buffer;
+}
+
+char *Base64Encode(const char *message)
+{
+    BIO *bio, *b64;
+    FILE *stream;
+
+    int encodedSize = 4 * ceil((double)strlen(message) / 3);
+
+    char *buffer = (char *)malloc(encodedSize + 1);
+
+    stream = fmemopen(buffer, encodedSize + 1, "w");
+    b64 = BIO_new(BIO_f_base64());
+    bio = BIO_new_fp(stream, BIO_CLOSE);
+
+    bio = BIO_push(b64, bio);
+
+    BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
+    BIO_write(bio, message, strlen(message));
+
+    BIO_flush(bio);
+    fclose(stream);
+
+    return buffer;
+}
+
+char *base64EncodeFile(const char *filePath)
+{
+    char *fileContents = readFile(filePath);
+
+    printf("%s\n", fileContents);
+
+    char *base64EncodeOutput = Base64Encode(fileContents);
+    return base64EncodeOutput;
+}
+
+char *base64EncodeIoStream()
+{
+    char fileContent[BUFFER_SIZE][BUFFER_SIZE];
+    char inputBuffer[BUFFER_SIZE];
+    unsigned int i = 0;
+
+    while (*inputBuffer != EOF)
+    {
+        fgets(inputBuffer, BUFFER_SIZE, stdin);
+        strcpy(fileContent[i], inputBuffer);
+        i++;
+    }
+
+    writeToFile(IO_STREAM_TEMP_FILE, fileContent);
+
+    char *base64Output = base64EncodeFile(IO_STREAM_TEMP_FILE);
+    deleteFile(IO_STREAM_TEMP_FILE);
+
+    return base64Output;
+}
+
+// this program requires openssl and should be compiled with -lcrypto -lm gcc compiler flags
+int main(int argc, const char *argv[])
+{
+    if (argc > 1)
+    {
+        if (strcmp(argv[1], "--version") == 0)
+        {
+            printf("%s\n", version);
+            return 0;
+        }
+        else if (strcmp(argv[1], "--help") == 0)
+        {
+            printf("%s\n", helpDocs);
+            return 0;
+        }
+    }
+    else
+    {
+        char *base64Encoding = base64EncodeIoStream();
+        printf("%s\n", base64Encoding);
+        return 0;
+    }
+
+    const char *filePath = argv[1];
+    char *base64Encoding = base64EncodeFile(filePath);
+    printf("%s\n", base64Encoding);
+
+    return 0;
+
+    // const char *filePath = argv[1];
+    // return base64EncodeFile(filePath);
+}
--- sort/sort.c
+++ sort/sort.c
@@ -6,8 +6,10 @@
 
 #define BUFFER_LENGTH 2056
 
-void printFileContent(char **fileContent) {
-    for (unsigned int i = 0; fileContent[i] != NULL; i++) {
+void printFileContent(char **fileContent)
+{
+    for (unsigned int i = 0; fileContent[i] != NULL; i++)
+    {
         // there is no need to include the newline character here since each line
         // should be at a different memory offset
         printf("%s", fileContent[i]);
@@ -15,18 +17,23 @@ void printFileContent(char **fileContent) {
 }
 
 // sort lines of the file using bubble sort
-char** sortFileContent(char **fileContent) {
+char **sortFileContent(char **fileContent)
+{
     int numLines = 0;
-    while (fileContent[numLines] != NULL) {
+    while (fileContent[numLines] != NULL)
+    {
         numLines++;
     }
 
-    for (int i = 0; i < numLines - 1; i++) {
-        for (int j = 0; j < numLines - i - 1; j++) {
-            if (strcmp(fileContent[j], fileContent[j+1]) > 0) {
+    for (int i = 0; i < numLines - 1; i++)
+    {
+        for (int j = 0; j < numLines - i - 1; j++)
+        {
+            if (strcmp(fileContent[j], fileContent[j + 1]) > 0)
+            {
                 char *temp = fileContent[j];
-                fileContent[j] = fileContent[j+1];
-                fileContent[j+1] = temp;
+                fileContent[j] = fileContent[j + 1];
+                fileContent[j + 1] = temp;
             }
         }
     }
@@ -34,23 +41,26 @@ char** sortFileContent(char **fileContent) {
     return fileContent;
 }
 
-char** readFileLines(char *fileName) {
-    FILE* filePointer;
+char **readFileLines(char *fileName)
+{
+    FILE *filePointer;
     char readBuffer[BUFFER_LENGTH];
     char **fileLines = NULL;
     int numLines = 0;
 
     filePointer = fopen(fileName, "r");
-    if (filePointer == NULL) {
+    if (filePointer == NULL)
+    {
         printf("Error: could not open file %s\n", fileName);
         return NULL;
     }
 
-    while(fgets(readBuffer, BUFFER_LENGTH, filePointer)) {
+    while (fgets(readBuffer, BUFFER_LENGTH, filePointer))
+    {
         numLines++;
-        fileLines = realloc(fileLines, numLines * sizeof(char*));
-        fileLines[numLines-1] = malloc(strlen(readBuffer)+1);
-        strcpy(fileLines[numLines-1], readBuffer);
+        fileLines = realloc(fileLines, numLines * sizeof(char *));
+        fileLines[numLines - 1] = malloc(strlen(readBuffer) + 1);
+        strcpy(fileLines[numLines - 1], readBuffer);
     }
 
     fclose(filePointer);
@@ -58,23 +68,30 @@ char** readFileLines(char *fileName) {
     return fileLines;
 }
 
-bool fileExists(char *filePath) {
-  struct stat buffer;
-  return stat(filePath, &buffer) == 0;
+bool fileExists(char *filePath)
+{
+    struct stat buffer;
+    return stat(filePath, &buffer) == 0;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc > 1) {
-        if (strcmp(argv[1], "--version") == 0) {
+int main(int argc, char *argv[])
+{
+    if (argc > 1)
+    {
+        if (strcmp(argv[1], "--version") == 0)
+        {
             printf("%s\n", version);
             return 0;
-        } else if (strcmp(argv[1], "--help") == 0) {
+        }
+        else if (strcmp(argv[1], "--help") == 0)
+        {
             printf("%s\n", helpDocs);
             return 0;
         }
 
         char *filePath = argv[1];
-        if (!fileExists(filePath)) {
+        if (!fileExists(filePath))
+        {
             printf("sort: cannot read: '%s': No such file or directory\n", filePath);
             return 2;
         }
@@ -83,14 +100,17 @@ int main(int argc, char *argv[]) {
         char **sortedFileContent = sortFileContent(fileContent);
 
         printFileContent(sortedFileContent);
-    } else {
+    }
+    else
+    {
         // read from stdin
         // this is most widely used in shell pipes
         char fileContent[BUFFER_LENGTH][BUFFER_LENGTH];
         char inputBuffer[BUFFER_LENGTH];
         unsigned int i = 0;
 
-        while(*inputBuffer != EOF) {
+        while (*inputBuffer != EOF)
+        {
             fgets(inputBuffer, BUFFER_LENGTH, stdin);
             strcpy(fileContent[i], inputBuffer);
             i++;
