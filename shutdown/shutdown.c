bb35574942b962873b206272c80669261779e47e
hudson-newey <neweyh@qut.edu.au>
2023-08-12 15:46:00 +1000

    Added reboot and shutdown commands

---
+++ shutdown/shutdown.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/reboot.h>
+#include "reboot.h"
+
+int main(int argc, const char *argv[]) {
+    if (argc > 1) {
+        if (strcmp(argv[1], "--version") == 0) {
+            printf("%s\n", version);
+            return 0;
+        } else if (strcmp(argv[1], "--help") == 0) {
+            printf("%s\n", helpDocs);
+            return 0;
+        }
+
+        return 1;
+    }
+
+    return reboot(RB_POWER_OFF);
+}
