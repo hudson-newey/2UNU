8c1608eb41a464ed6ec1a77fb083c89cbd4aa05a
hudson-newey <hudsonnewey@gmail.com>
2023-09-13 21:05:35 +1000

    Fix running multiple commands in sh

--- sh/sh.c
+++ sh/sh.c
@@ -12,46 +12,61 @@ bool fileExists(char *filename)
     return (stat(filename, &buffer) == 0);
 }
 
+bool directoryExists(char *path)
+{
+    struct stat buffer;
+    return (stat(path, &buffer) == 0);
+}
+
 // some commands (such as cd) are sh commands and not their own applications
 bool isProgramCommand(char *command)
 {
-    // char *programCommands[] = {
-    //     "cd"};
+    char *programCommands[] = {"cd", "exit"};
 
-    // for (int i = 0; i < sizeof(programCommands); i++)
-    // {
-    //     if (strcmp(programCommands[i], command) == 0)
-    //     {
-    //         return true;
-    //     }
-    // }
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(programCommands[i], command) == 0)
+        {
+            return true;
+        }
+    }
 
     return false;
 }
 
 int runCommand(char *currentPathHead, char *command, char *commandArgs)
 {
-    bool commandFound = false;
-    int commandReturnCode = 0;
-
     // search through current directory
     char *currentDirectoryCommand = strcat(currentPathHead, command);
-    if (fileExists(currentDirectoryCommand) && !commandFound)
+    if (fileExists(currentDirectoryCommand))
     {
         char *constructedCommand = strcat(strcat(command, " "), commandArgs);
-        system(constructedCommand);
-        commandFound = true;
+        return system(constructedCommand);
     }
 
-    if (!commandFound) {
-        printf("sh: command not found: %s\n", command);
-        return 1;
-    }
+    printf("sh: command not found: %s\n", command);
+    return 1;
 }
 
-int executeProgramCommand(char *command)
+int executeProgramCommand(char *command, char *commandArgs, char *pathHead)
 {
-    return 0;
+    if (strcmp(command, "cd") == 0)
+    {
+        if (directoryExists(commandArgs))
+        {
+            strcpy(pathHead, commandArgs);
+            return 0;
+        }
+
+        printf("sh: cd: %s: No such file or directory\n", commandArgs);
+        return 1;
+    }
+    else if (strcmp(command, "exit") == 0)
+    {
+        exit(0);
+    }
+
+    return 1;
 }
 
 int runFromIoStream(const char *contents)
@@ -67,28 +82,40 @@ int runFromIoStream(const char *contents)
 
 int readFromInput()
 {
+    char currentPathHead[PATHS_BUFFER] = "./";
     while (true)
     {
-        char currentPathHead[256] = "./";
-        char command[256] = "";
-        char commandArgs[256] = "";
+        char command[COMMAND_BUFFER] = "";
+        char commandArgs[COMMAND_BUFFER] = "";
 
         printf("\nsh-%s%s", version, prompt);
-        char input[512];
-        fgets(input, 512, stdin);
+        char input[COMMAND_BUFFER];
+        fgets(input, COMMAND_BUFFER, stdin);
         sscanf(input, "%s %s", command, commandArgs);
-        if (commandArgs[0] == '\0') {
+        if (commandArgs[0] == '\0')
+        {
             strcpy(commandArgs, "");
         }
 
         // cd command
         if (isProgramCommand(command))
         {
-            strcpy(currentPathHead, commandArgs);
+            executeProgramCommand(command, commandArgs, currentPathHead);
+            printf("head: %s\n", currentPathHead);
         }
         else
         {
-            runCommand(currentPathHead, command, commandArgs);
+            // we have to do this otherwise when we use strcat later on its done by reference
+            // we instead want to use the value of the variable
+            char commandPathHead[PATHS_BUFFER] = "";
+            char commandValue[COMMAND_BUFFER] = "";
+            char commandArgsValue[COMMAND_BUFFER] = "";
+
+            strcpy(commandPathHead, currentPathHead);
+            strcpy(commandValue, command);
+            strcpy(commandArgsValue, commandArgs);
+
+            runCommand(commandPathHead, commandValue, commandArgsValue);
         }
     }
     return 1;
--- sh/sh.h
+++ sh/sh.h
@@ -1,8 +1,11 @@
+#define PATHS_BUFFER 256
+#define COMMAND_BUFFER 2048
+
 const char *version = "0.0.2";
 const char *prompt = "$ ";
 const char *helpDocs = "TODO: When man command is completed";
 
-char paths[256][256] = {
+const char paths[PATHS_BUFFER][PATHS_BUFFER] = {
     "~/.local/bin/",
     "/usr/local/bin/",
     "/usr/bin/",
